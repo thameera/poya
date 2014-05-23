bcc5d06dbcf4adb743b48f19ad644c75e4a1a957
Malith Senaweera <malithsenaweera@gmail.com>
2014-05-23 17:04:25 +0530

    Add an API

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
-from views import nextpoya
+from views import mainView, listAllPoya, next
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    ('^$', nextpoya),
+    url('^$', mainView),
+    url(r'^api/list$', listAllPoya),
+    url(r'^api/next$', next),
 )
--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.utils import simplejson
 from datetime import datetime, date, timedelta
 import socket
 
-def nextpoya(request):
+def poyastats(month=None):
     ctx = {}
 
     cur = datetime.now()
@@ -10,33 +12,41 @@ def nextpoya(request):
         # Offset time in app engine
         cur = cur + timedelta(hours=5,minutes=30)
 
-    # ctx['time'] = cur
-    today = cur.date()
-    poya = getnextpoya(today)
+    day = cur.date()
+    if month:
+        poya = getPoyaByMonth(month)
+    else:
+        poya = getnextpoya(day)
 
     if poya:
         week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        ctx['month'] = months[poya.month - 1]
         ctx['date'] = '%s' %poya
-        ctx['days'] = (poya - today).days
+        ctx['days'] = (poya - day).days
         ctx['weekday'] = week[ poya.weekday() ]
-        if poya.weekday() < 5:
-            ctx['emoticon'] = ':)'
-            ctx['happy'] = True;
-        else:
-            ctx['emoticon'] = ':('
-            ctx['happy'] = False;
+    return [poya, ctx]
 
-        if ctx['days'] > 1:
-            ctx['daysuffix'] = 's'
-        else:
-            ctx['daysuffix'] = ''
+def mainView(request):
+    poya, ctx = poyastats()
+    if poya.weekday() < 5:
+        ctx['emoticon'] = ':)'
+        ctx['happy'] = True;
+    else:
+        ctx['emoticon'] = ':('
+        ctx['happy'] = False;
 
-        if poya.month == 9:
-            ctx['tip'] = "Random Poya day tip: Visit the Colombo International Book Fair at BMICH!"
+    if ctx['days'] > 1:
+        ctx['daysuffix'] = 's'
+    else:
+        ctx['daysuffix'] = ''
+
+    if poya.month == 9:
+        ctx['tip'] = "Random Poya day tip: Visit the Colombo International Book Fair at BMICH!"
 
     return render_to_response('index.html', ctx)
 
-def getnextpoya(today):
+def getnextpoya(day):
     try:
         f = open('poyadays.txt')
         dates = f.readlines()
@@ -45,9 +55,35 @@ def getnextpoya(today):
         for zdt in dates:
             zdt = zdt.strip()
             dt = datetime.strptime(zdt, '%d %m %Y').date()
-            if dt > today:
+            if dt > day:
                 return dt
     except:
         return None
     return None
 
+def getPoyaByMonth(month):
+    try:
+        f = open('poyadays.txt')
+        dates = f.readlines()
+        f.close()
+
+        for zdt in dates:
+            zdt = zdt.strip()
+            dt = datetime.strptime(zdt, '%d %m %Y').date()
+            if dt.month == month:
+                return dt
+    except:
+        return None
+    return None
+
+### API ###
+def listAllPoya(request):
+    di = {}
+    for m in range(1,13):
+        ctx = poyastats(m)[1]
+        di[ctx['month']] = ctx
+    return HttpResponse(simplejson.dumps(di), mimetype="application/json")
+
+def next(request):
+    ctx = poyastats()[1]
+    return HttpResponse(simplejson.dumps(ctx), mimetype="application/json")
